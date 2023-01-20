73de7192ce21a6006ce7f2bfc59800b2246aa296
Billy Hsu <ga017281@gmail.com>
2023-01-20 11:18:57 +0800

    add split_strings

---
+++ split_strings.py
@@ -0,0 +1,24 @@
+def solution(s):
+    arr=[]
+    i=0
+    while i < len(s)-1:
+        arr.append(s[i]+s[i+1])
+        i+=2
+    if len(s)%2 != 0:
+        arr.append(f"{s[-1]}_")
+    print(arr)
+    return arr
+
+
+# def solution(s):
+#     result = []
+#     if len(s) % 2:
+#         s += '_'
+#     for i in range(0, len(s), 2):
+#         result.append(s[i:i+2])
+#     return result
+# this is the more efficient solution by using for range loop
+        
+
+solution("zaqxswcde") #['za', 'qx', 'sw', 'cd', 'e_']
+solution("ABC")#['AB', 'C_']
