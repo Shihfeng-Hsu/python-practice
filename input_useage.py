ce2b1f27534aac56cb2336a14691d1a0af0342da
Billy Hsu <ga017281@gmail.com>
2023-01-23 14:59:21 +0800

    add input_useage.py

---
+++ input_useage.py
@@ -0,0 +1,8 @@
+num = input("number") # input 12
+print(type(num)) #<class 'str'>
+print(type(int(num))) #<class 'int'>
+
+counter = input("expression") # 9+5
+print(counter) #9+9
+print(eval(counter)) # 18
+#eval = evaluation 
